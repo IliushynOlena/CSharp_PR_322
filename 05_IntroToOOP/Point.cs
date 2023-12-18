921bda1d8e2818347972a1d4fa9a306b21a20893
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-12-18 11:00:49 +0200

    Classes, Struct, Properties

---
+++ 05_IntroToOOP/Point.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_IntroToOOP
+{
+    partial class Point
+    {
+        public void SetX(int newX)
+        {
+            if (newX >= 0)
+                this._xCoord = newX;
+            else
+                _xCoord = 0;
+        }
+        public void SetY(int newY)
+        {
+            if (newY >= 0)
+                this.yCoord = newY;
+            else
+                yCoord = 0;
+        }
+        public int getX() { return _xCoord; }
+        public int getY() { return yCoord; }
+    }
+    partial class Point
+    {
+        public void Print()
+        {
+            Console.WriteLine($"X : {_xCoord}. Y : {yCoord}");
+        }
+        public override string ToString()
+        {
+            return $"X : {_xCoord}. Y : {yCoord}";
+        }
+    }
+    partial class Point
+    {
+
+        public Point() : this(0, 0)
+        {
+            //default values:
+            //number : 0
+            //bolean : false
+            //references : null
+
+            //_xCoord = 0;
+            //yCoord = 0;
+        }
+        public Point(int value) : this(value, value) { }
+        public Point(int x, int y)
+        {
+            XCoord = x;
+            YCoord = y;
+            count++;
+        }
+    }
+}
+
+namespace _05_IntroToOOP
+{
+    partial class Point
+    {
+        public void PrintWithSetPosition()
+        {
+            Console.SetCursorPosition(XCoord, YCoord);
+            Print();
+        }
+    }
+}
---
+++ 05_IntroToOOP/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.Xml.Linq;
+
+namespace _05_IntroToOOP
+{
+    //Access Spetifacators
+    /*
+    - private (default for field)
+    - protected
+    - public
+    - protected internal
+    - internal
+     
+     */
+    class MyClass :Object
+    {
+        //private
+        private int number;
+        private string name;
+        private const float PI = 3.14f;
+        private readonly int id;
+        public MyClass()
+        {
+            id = 10;
+            name = "MyClass";
+        }
+        //void SetId(int id) 
+        //{ 
+        //    this.id = id;
+        //}
+        public void Print()
+        {
+            Console.WriteLine($"Id : {id} Name {name}");
+        }
+        public override string ToString()
+        {
+            return $"Id : {id} Name {name}";
+        }
+    }
+    partial class Point
+    {
+        //Auto-property   prop+Tab
+        public string Name { get; set; }
+        public string Type { get; }//readonly
+        //==========
+        //FULL- property
+        //private string name;
+        //public string Name
+        //{
+        //    get 
+        //    { 
+        //        return name;
+        //    }
+        //    set 
+        //    { 
+        //        name = value;
+        //    }
+        //}
+
+
+
+        private int _xCoord;
+        public int XCoord//value
+        {
+            get
+            {
+                return _xCoord;
+            }
+            set
+            {
+                if (value >= 0)
+                    this._xCoord = value;
+                else
+                    _xCoord = 0;
+            }
+        }
+        //full property  propfull+Tab
+        private int yCoord;
+        public int YCoord
+        {
+            get { return yCoord; }
+            set
+            {
+                if (value >= 0)
+                    this.yCoord = value;
+                else
+                    yCoord = 0;
+            }
+        }
+        public string Address { get; private set; } = "Soborna";
+
+        static int count;
+        static Point()
+        {
+            count = 0;
+        }
+        public static int getCount()
+        {
+            return count;
+        }
+
+
+    }
+    //references type
+    class DerivedClass : MyClass
+    { }
+    //value type 
+    internal struct MyStruct
+    {
+        public int x;
+        public int y;
+        public void Print()
+        {
+            Console.WriteLine($"X : {x}. Y : {y}");
+        }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Console.SetCursorPosition(10, 10);
+            Point p = new Point(5,9);  
+            p.Print();
+            Console.WriteLine(p);
+            p.SetX(10);
+            p.SetY(21);
+            Console.WriteLine(p);
+            Console.WriteLine($"X = {p.getX()}"  );
+            Console.WriteLine($"Y = {p.getY()}"  );
+            //XCoord = value
+            p.XCoord = 5; //setter
+            int x = p.XCoord;
+            Console.WriteLine(x);//getter
+            p.Name = "2D_Point";//setter
+            Console.WriteLine(p.Name);//getter
+
+            Console.WriteLine(Point.getCount());
+
+            p.PrintWithSetPosition();
+
+           // MyClass @class = new MyClass();
+           //MyClass @int = new MyClass();
+           //MyClass @float = new MyClass();
+
+           // @class.Print();
+           // Console.WriteLine(@class.ToString());
+        }
+    }
+}
+
+
